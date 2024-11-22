b785e6f48fc2f1a4b55fd873f86c6bf17b15f4b4
Bektur Razzakov <brzzkv@gmail.com>
2024-11-23 01:04:40 +0600

    alert icon

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import moment from 'moment';
 import 'moment/locale/ru';
 import { Route, Routes } from 'react-router-dom';
-import './App.css';
 import Home from './Containers/Home/Home';
 import Header from './Components/Header/Header';
+import Alerts from './Components/Alerts/Alerts';
+import './App.css';
 
 moment.locale('ru');
 
 const App = () => (
   <div className="App">
     <Header />
+    <Alerts />
     <Routes>
       <Route path="home" element={<Home />} />
     </Routes>
